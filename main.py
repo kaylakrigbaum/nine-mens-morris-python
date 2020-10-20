4aa536b57ad0a8e2ecfb175104649ac16b0026ba
Kayla Krigbaum <47722031+kaylakrigbaum@users.noreply.github.com>
2020-10-20 14:16:45 -0500

    mostly converted

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import random
 
 #menu
 def menu():
-    print("Would you like to begin the game?")
-    #come back and regulate input types
-    menuInput = input("Enter Y or N")
-    if (menuInput == "Y"):
+    print('Would you like to begin the game?')
+    while True:
+        try:
+            choice = str(raw_input('Enter Y or N ==> ')).lower()
+            if (choice != 'y') and (choice != 'n'):
+                raise ValueError
+            break
+        except ValueError:
+            print('Please enter a single Y or N character')
+
+    if choice == 'y':
         return True
     return False
 
@@ -16,8 +23,81 @@ def selectPlayerTurn():
     return turn
 
 #check for any mills
-def checkForMill():
-    return
+def checkForMill(board, check, currentPlayer):
+    #top horizontal
+    if (board[0][0] == board[0][3] and board[0][0] == board[0][6] and board[0][0] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    #left vertical
+    elif (board[1][1] == board[1][3] and board[1][1] == board[1][5] and board[1][1] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    #bottom horizontal
+    elif (board[2][2] == board[2][3] and board[2][2] == board[2][4] and board[2][2] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    #bottom horizontal
+    elif (board[2][2] == board[2][3] and board[2][2] == board[2][4] and board[2][2] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[3][0] == board[3][1] and board[3][0] == board[3][2] and board[3][0] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[3][4] == board[3][5] and board[3][4] == board[3][6] and board[3][4] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[4][2] == board[4][3] and board[4][2] == board[4][4] and board[4][2] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[5][1] == board[5][3] and board[5][1] == board[5][5] and board[5][1] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[6][0] == board[6][3] and board[6][0] == board[6][6] and board[6][0] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[0][0] == board[3][0] and board[0][0] == board[6][0] and board[0][0] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[1][1] == board[3][1] and board[1][1] == board[5][1] and board[1][1] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[2][2] == board[3][2] and board[2][2] == board[4][2] and board[2][2] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[0][3] == board[1][3] and board[0][3] == board[2][3] and board[0][3] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[4][3] == board[5][3] and board[4][3] == board[6][3] and board[4][3] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[2][4] == board[3][4] and board[2][4] == board[4][4] and board[2][4] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[1][5] == board[3][5] and board[1][5] == board[5][5] and board[1][5] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    elif (board[0][6] == board[3][6] and board[0][6] == board[6][6] and board[0][6] != 0):
+        print(currentPlayer, " HAS CREATED A MILL")
+        return True
+
+    else:
+        return False
 
 #output the current board
 def displayBoard(board, rows, cols):
@@ -28,11 +108,11 @@ def displayBoard(board, rows, cols):
     return
 
 #test board creation
-def testBoardCreation():
-    return
+def testBoardCreation(board, size):
+    return board.size() == size
 
 #test mills
-def testMills():
+def testMills(board):
     return
 
 #phase 1: players place their pieces on the board
@@ -68,7 +148,9 @@ def main():
         displayBoard(board, rows, cols)
 
         #declare player objects
-        p1, p2 = player()
+        p1 = player()
+        p2 = player()
+
 
         #randomize turn
         playerTurn = selectPlayerTurn()
