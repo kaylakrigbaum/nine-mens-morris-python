7e97c11d48a806bcd79338ce25b3c1cb91211992
Ben Morgan <morganbenjamin968@gmail.com>
2020-11-19 19:31:56 -0600

    Created bogus tests, under normal circumstances when pygame isn't being a shitter, this should work

--- UnitTests.py
+++ UnitTests.py
@@ -1,1 +1,22 @@
+import unittest
+import main
 
+class TestGameAspects(unittest.TestCase):
+    def test_playing_true(self):
+        self.assertEqual(main.playing, True)
+
+    def test_phase1_initiated(self):
+        self.assertTrue(main.drop_piece(), True)
+
+    def test_phase2_initiated(self):
+        self.assertEqual(main.p1.placedPieces, 9, 'Player 1 has placed all nine pieces')
+        self.assertEqual(main.p2.placedPieces, 9, 'Player 2 has placed all nine pieces')
+
+    def test_phase3_initiated(self):
+        self.assertEqual((main.p1.pieceCount, 3, 'Player 1 has reached 3 remaining pieces') or self.assertEqual(main.p2.pieceCount, 3, 'Player 2 has reached 3 remaining pieces'))
+
+
+def mainTwo():
+    unittest.main()
+if __name__ == '__main__':
+    mainTwo()
